8a83323154f92aa4dc60e8d9506ee46bade7ff11
jkwu <jkwu@amarsoft.com>
2018-06-24 22:01:32 +0800

    feat: modify base env

--- src/studyPractise/es6/promise.js
+++ src/studyPractise/es6/promise.js
@@ -67,21 +67,21 @@ log = data => console.log(data);
 // });
 
 //
-// const p1 = new Promise((resolve, reject) => {
-//   resolve('hello');
-// })
-//   .then(result => result)
-//   .catch(e => e);
-//
-// const p2 = new Promise((resolve, reject) => {
-//   throw new Error('p2:报错了!');
-// })
-//   .then(result => result)
-//   .catch(e => e);
-//
-// Promise.all([p1, p2])
-//   .then(result => console.log(result))
-//   .catch(e => console.log('all:' + e));
+const p1 = new Promise((resolve, reject) => {
+  resolve('hello');
+})
+  .then(result => result)
+  .catch(e => e);
+
+const p2 = new Promise((resolve, reject) => {
+  throw new Error('p2:报错了!');
+})
+  .then(result => result)
+  .catch(e => e);
+
+Promise.all([p1, p2])
+  .then(result => console.log(result))
+  .catch(e => console.log('all:' + e));
 
 // function getFoo () {
 //   return new Promise(function (resolve, reject){
